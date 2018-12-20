bef3c4f97866f252fbceded2b90f13dd2e4b7313
rob137 <robertaxelkirby@gmail.com>
2018-12-20 10:49:29 +0000

    Update ContactData interface and add NewContact interface

--- src/types.ts
+++ src/types.ts
@@ -1,11 +1,20 @@
 export type ContactData = Contact[];
 
 export type Contact = {
-  "name": string;
-  "phone": string;
-  "company": string;
-  "position": string;
-  "status": string;
-  "lastContact": string;
-  "notes": string;
+  name: string;
+  phone: string;
+  company: string;
+  position: string;
+  status: string;
+  lastContact: string;
+  notes: string;
+}
+
+export type NewContact = {
+  name: string;
+  phone: string;
+  company: string;
+  position: string;
+  lastContact: string;
+  notes: string;
 }
