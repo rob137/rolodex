120d2f7b3ded6760991a136ed12bf3502970f874
rob137 <robertaxelkirby@gmail.com>
2018-12-20 11:20:45 +0000

    Make ContactAdder stateful

--- src/ContactAdder.tsx
+++ src/ContactAdder.tsx
@@ -1,18 +1,45 @@
 import React, { Component } from 'react';
 
-export default function ContactAdder() {
-  return (
-    <form onSubmit={(e) => {
-      e.preventDefault();
-      return false;
-    }}>
-      <input type="text" name="name" placeholder="Name"/>
-      <input type="text" name="phone" placeholder="Phone"/>
-      <input type="text" name="company" placeholder="Company"/>
-      <input type="text" name="position" placeholder="Position"/>
-      <input type="text" name="lastContact" placeholder="Last contacted"/>
-      <input type="text" name="notes" placeholder="Notes"/>
-      <input type="submit"/>
-    </form>
-  )
+interface ContactAdderState {
+  name: string;
+  phone: string;
+  company: string;
+  position: string;
+  lastContact: string;
+  notes: string;
 }
+
+export default class ContactAdder extends Component {
+  constructor(props: {}) {
+    super(props);
+    this.state = {
+      name: '',
+      phone: '',
+      company: '',
+      position: '',
+      lastContact: '',
+      notes: '',
+    };
+  }
+
+  render() {
+    return (
+      <form onSubmit={(e) => {
+        e.preventDefault();
+        return false;
+      }}>
+        <input type="text" name="name"   placeholder="Name"/>
+        <input type="text" name="phone"  placeholder="Phone"/>
+        <input type="text" name="company"  placeholder="Company"/>
+        <input type="text" name="position"   placeholder="Position"/>
+        <input type="text" name="lastContact"  placeholder="Last contacted"/>
+        <input type="text" name="notes" placeholder="Notes"/>
+        <input type="submit"/>
+      </form>
+    );
+  }
+}
+
+/* 
+On submitting, I want the form to clear and all the props to be saved in a new row at the bottom of the table 
+*/
