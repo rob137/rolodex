399c663ede233f4417fc8e603c811cc8d2952d04
rob137 <robertaxelkirby@gmail.com>
2018-12-20 10:16:04 +0000

    Add headers to ContactList

--- src/ContactList.tsx
+++ src/ContactList.tsx
@@ -11,6 +11,16 @@ export default function ContactList(props: ContactListProps) {
   return (
     <table>
       <tbody>
+        <tr>
+          <th/>
+          <th>Name</th>
+          <th>Phone</th>
+          <th>Company</th>
+          <th>Position</th>
+          <th>Status</th>
+          <th>Last Contacted</th>
+          <th>Notes</th>
+        </tr>
         {
           props.contactData.map((contact: Contact, key) => {
             return <ContactRow contact={contact} key={key}/>
