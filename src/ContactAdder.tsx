30d72be62961de516ddc23b4959b60be1cb62eb3
rob137 <robertaxelkirby@gmail.com>
2018-12-20 11:06:47 +0000

    Add form field names to ContactAdder

--- src/ContactAdder.tsx
+++ src/ContactAdder.tsx
@@ -6,12 +6,12 @@ export default function ContactAdder() {
       e.preventDefault();
       return false;
     }}>
-      <input type="text" placeholder="Name"/>
-      <input type="text" placeholder="Phone"/>
-      <input type="text" placeholder="Company"/>
-      <input type="text" placeholder="Position"/>
-      <input type="text" placeholder="Last contact"/>
-      <input type="text" placeholder="Notes"/>
+      <input type="text" name="name" placeholder="Name"/>
+      <input type="text" name="phone" placeholder="Phone"/>
+      <input type="text" name="company" placeholder="Company"/>
+      <input type="text" name="position" placeholder="Position"/>
+      <input type="text" name="lastContact" placeholder="Last contacted"/>
+      <input type="text" name="notes" placeholder="Notes"/>
       <input type="submit"/>
     </form>
   )
