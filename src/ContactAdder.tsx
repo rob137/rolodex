c83b2340f820da8049f0a69ac1ea13caa28f87d7
rob137 <robertaxelkirby@gmail.com>
2018-12-20 11:43:27 +0000

    Save text input to state of ContactAdder

--- src/ContactAdder.tsx
+++ src/ContactAdder.tsx
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 
+import { ContactKey } from './types';
+
 interface ContactAdderState {
   name: string;
   phone: string;
@@ -9,7 +11,7 @@ interface ContactAdderState {
   notes: string;
 }
 
-export default class ContactAdder extends Component {
+export default class ContactAdder extends Component<{}, ContactAdderState> {
   constructor(props: {}) {
     super(props);
     this.state = {
@@ -22,18 +24,58 @@ export default class ContactAdder extends Component {
     };
   }
 
+  handleChange(fieldName: ContactKey, input: string) {
+    this.setState({ [fieldName]: input } as any);
+  }
+
   render() {
     return (
       <form onSubmit={(e) => {
         e.preventDefault();
         return false;
       }}>
-        <input type="text" name="name"   placeholder="Name"/>
-        <input type="text" name="phone"  placeholder="Phone"/>
-        <input type="text" name="company"  placeholder="Company"/>
-        <input type="text" name="position"   placeholder="Position"/>
-        <input type="text" name="lastContact"  placeholder="Last contacted"/>
-        <input type="text" name="notes" placeholder="Notes"/>
+        <input
+          type="text"
+          onChange={(e) => this.handleChange(e.target.name as ContactKey, e.target.value)}
+          value={this.state.name}
+          name="name"
+          placeholder="Name"
+        />
+        <input
+          type="text"
+          onChange={(e) => this.handleChange(e.target.name as ContactKey, e.target.value)}
+          value={this.state.phone}
+          name="phone"
+          placeholder="Phone"
+        />
+        <input
+          type="text"
+          onChange={(e) => this.handleChange(e.target.name as ContactKey, e.target.value)}
+          value={this.state.company}
+          name="company"
+          placeholder="Company"
+        />
+        <input
+          type="text"
+          onChange={(e) => this.handleChange(e.target.name as ContactKey, e.target.value)}
+          value={this.state.position}
+          name="position"
+          placeholder="Position"
+        />
+        <input
+          type="text"
+          onChange={(e) => this.handleChange(e.target.name as ContactKey, e.target.value)}
+          value={this.state.lastContact}
+          name="lastContact"
+          placeholder="Last contacted"
+        />
+        <input
+          type="text"
+          onChange={(e) => this.handleChange(e.target.name as ContactKey, e.target.value)}
+          value={this.state.notes}
+          name="notes"
+          placeholder="Notes"
+        />
         <input type="submit"/>
       </form>
     );
