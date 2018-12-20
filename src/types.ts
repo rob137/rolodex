1cf607e32165b4a8bfe47f1c03306a40445e727b
rob137 <robertaxelkirby@gmail.com>
2018-12-20 09:55:42 +0000

    Add types.ts

---
+++ src/types.ts
@@ -0,0 +1,11 @@
+export type ContactData = Contact[];
+
+type Contact = {
+  "name": string;
+  "phone": string;
+  "company": string;
+  "position": string;
+  "status": string;
+  "lastContact": string;
+  "notes": string;
+}
