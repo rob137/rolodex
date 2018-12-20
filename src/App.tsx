f9979d2a8bcc58d3f1772f0d88d3e1d85d709d01
rob137 <robertaxelkirby@gmail.com>
2018-12-20 10:21:58 +0000

    Remove more react 'hello world' bits

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import './App.css';
-import logo from './logo.svg';
 
 import ContactList from './ContactList';
 import contactData from './contact-data.json';
@@ -20,16 +19,7 @@ class App extends Component<{}, AppState> {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
           <ContactList contactData={this.state.contactData}/>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
         </header>
       </div>
     )
